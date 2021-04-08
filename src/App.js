2e6479057a88cf864531e5db77f9ebf5478e83d1
p22.araujo <p22.araujo@webmotors.com.br>
2021-04-07 22:24:27 -0300

    Modulo 5 - Aula 35

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import NewPlace from './places/pages/NewPlace';
@@ -7,23 +7,36 @@ import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Auth from './user/pages/Auth';
 import Users from './user/pages/User';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
 
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  
+  const login = useCallback(() => {
+    setisLoggedIn(true);
+  },[]);
+
+  const logout = useCallback(() => {
+    setisLoggedIn(false);
+  },[]);
+
   return (
-    <BrowserRouter>
-      <MainNavigation/>
-      <main>
-        <Switch>
-          <Route path="/" component={Users} exact={true}/>
-          <Route path="/:userId/places" component={UserPlaces} exact={true} />
-          <Route path="/places/new" component={NewPlace} exact={true}/>
-          <Route path="/places/:placeId" component={UpdatePlace} exact={true}/>
-          <Route path="/auth" component={Auth} exact={true}/>
-          <Redirect to="/"/>
-        </Switch>
-      </main>  
-    </BrowserRouter>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+      <BrowserRouter>
+        <MainNavigation/>
+        <main>
+          <Switch>
+            <Route path="/" component={Users} exact={true}/>
+            <Route path="/:userId/places" component={UserPlaces} exact={true} />
+            <Route path="/places/new" component={NewPlace} exact={true}/>
+            <Route path="/places/:placeId" component={UpdatePlace} exact={true}/>
+            <Route path="/auth" component={Auth} exact={true}/>
+            <Redirect to="/"/>
+          </Switch>
+        </main>  
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
